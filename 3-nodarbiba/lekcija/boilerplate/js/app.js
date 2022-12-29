eb24cc65258ee4f6c9396ad3bd3f5005c1a3dc26
Rudolfs Dudarevs <rudolfs.dudarevs@ideaportriga.com>
2022-12-29 19:13:52 +0200

    3. nodarbiba

---
+++ 3-nodarbiba/lekcija/boilerplate/js/app.js
@@ -0,0 +1,9 @@
+const menuIcon = document.getElementById("menu-icon");
+const menu = document.getElementById("menu");
+
+const toggleMenu = () => {
+    
+}
+
+
+menuIcon.addEventListener("click", )
