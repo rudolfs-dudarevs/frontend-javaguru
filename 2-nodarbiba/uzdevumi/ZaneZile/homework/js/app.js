79cdb011a63b7e375c39faf0d834461ebeb43008
zaneziile <zane.ozoolina@gmail.com>
2023-01-02 22:24:57 +0200

    2: Zane Zile - Majasdarbs

---
+++ 2-nodarbiba/uzdevumi/ZaneZile/homework/js/app.js
@@ -0,0 +1,9 @@
+const actionBtn = document.getElementById("action-btn");
+
+const changeStyle = () =>{
+    actionBtn.style.backgroundColor = "green";
+    actionBtn.style.width = "505px";
+    actionBtn.style.height = "145px";
+}
+
+
